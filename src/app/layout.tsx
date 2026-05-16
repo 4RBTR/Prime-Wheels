1d8a67cbb122f4221e485a15325b92fed83f500b
4RBTR <andradanendrabagas@gmail.com>
2026-05-16 19:33:34 +0700

    fix: add missing Vercel analytics and speed insights imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "react-toastify/ReactToastify.css";
 import AdminSidebar from "@/components/AdminSidebar";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 
 const geistSans = Geist({
